ea777169d993cef05cb12d8e29a35ab8e1356159
BWinninger <bwinninger@gmail.com>
2015-12-13 19:17:29 -0500

    Set up protractor config file, added end to end tests for feedback, changed feedback routes to prevent unauthorized access

--- modules/feedback/tests/e2e/feedback.e2e.tests.js
+++ modules/feedback/tests/e2e/feedback.e2e.tests.js
@@ -2,9 +2,62 @@
 
 describe('Feedback E2E Tests:', function() {
 	describe('Test Feedback page', function() {
-		it('Should not include new Feedback', function() {
-			browser.get('http://localhost:3000/#!/feedback');
-			expect(element.all(by.repeater('feedback in feedback')).count()).toEqual(0);
+		it('Guests should be required to sign in to create feedback', function() {
+			browser.get('http://localhost:3000/feedback/');
+			expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/authentication/signin');
 		});
+
+
+		it('Guests should be able to sign in to an account', function() {
+			element(by.id('username')).clear();
+      element(by.id('password')).clear();
+			element(by.id('username')).sendKeys('notadmin');
+			element(by.id('password')).sendKeys('Password1!');
+			browser.driver.findElement(by.xpath("//button[text() = 'Sign in']")).click();
+
+			expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/feedback/');
+		});
+
+		it('Users should be able to send feedback', function() {
+		 browser.get('http://localhost:3000/feedback/create');
+		 element(by.id('title')).clear();
+		 element(by.id('feedback-text')).clear();
+		 element(by.id('title')).sendKeys("User automated feedback text");
+		 element(by.id('feedback-text')).sendKeys("Protractor feedback!");
+
+		 expect(browser.driver.findElement(by.xpath("//input[@type='submit']")).click());
+		});
+
+		it('Users should not be able to view the feedback list', function() {
+			browser.get('http://localhost:3000/feedback');
+			expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/forbidden');
+			browser.get('http://localhost:3000/api/auth/signout');
+		});
+
+		it('Admins should be able to send feedback', function() {
+			browser.get('http://localhost:3000/authentication/signin');
+
+			element(by.id('username')).clear();
+			element(by.id('password')).clear();
+			element(by.id('username')).sendKeys('a');
+			element(by.id('password')).sendKeys('qwertyuiop1234567890');
+			browser.driver.findElement(by.xpath("//button[text() = 'Sign in']")).click();
+
+			expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/');
+
+			browser.get('http://localhost:3000/feedback/create');
+			element(by.id('title')).clear();
+			element(by.id('feedback-text')).clear();
+			element(by.id('title')).sendKeys("Admin automated feedback text");
+			element(by.id('feedback-text')).sendKeys("Protractor feedback!");
+
+			expect(browser.driver.findElement(by.xpath("//input[@type='submit']")).click());
+		});
+
+		it('Admins should be able to view the feedback list', function() {
+			expect(browser.get('http://localhost:3000/feedback'));
+		});
+
+
 	});
 });
--- protractor.conf.js
+++ protractor.conf.js
@@ -2,5 +2,9 @@
 
 // Protractor configuration
 exports.config = {
-  specs: ['modules/*/tests/e2e/*.js']
+  //specs: ['modules/*/tests/e2e/*.js']
+  baseUrl: 'http://localhost:3000/',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  framework: 'jasmine',
+  specs: ['modules/feedback/tests/e2e/*.js']
 };
